b12de140f835ea286378138d980359957acde42f
DHkim <ultraraptor@naver.com>
2020-06-08 20:17:08 +0900

    good!

---
+++ Raspi4_Opencv4_setup_bin.sh
@@ -0,0 +1,14 @@
+sudo apt-get update 
+sudo apt-get -y install build-essential cmake pkg-config 
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install libfontconfig1-dev libcairo2-dev
+sudo apt-get -y install libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt-get -y install libgtk2.0-dev libgtk-3-dev
+
+sudo apt-get -y install libatlas-base-dev gfortran
+sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get -y install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+pip3 install imutils
+pip3 install opencv-contrib-python==4.1.0.25
